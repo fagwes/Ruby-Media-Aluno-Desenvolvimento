6990169b952f51e130ada7c9417230a373315ae3
fagwes <109398136+fagwes@users.noreply.github.com>
2023-03-28 13:22:51 -0300

    Create Media.rb
    Media de Alunos

---
+++ Media.rb
@@ -0,0 +1,17 @@
+nome = 'Wesley'
+puts('Aluno:',nome)
+
+
+nota1 = 7
+nota2 = 8
+nota3 = 5
+
+media = (nota1+nota2+nota3)/3
+falta = 9
+
+
+if media>=5 and falta<=5
+  puts ('Aprovado')
+else
+  puts ('Reprovado')
+end  
